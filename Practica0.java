1c36717b247e29fa6a0c45621fbf491fd901fac0
Victor <vmd1977@hotmail.com>
2021-09-27 18:19:20 -0500

    Nuevos cambios

--- Practica0.java
+++ Practica0.java
@@ -1,5 +1,6 @@
-public class Practica0{
-    public static void main(String[] args){
+public class Practica0 {
+    public static void main(String[] args) {
         System.out.println("Hola");
+        System.out.println("Hola mundo");
     }
 }
